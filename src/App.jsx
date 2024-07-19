3c3467dfc5a96fa1a08ea51d3714cd2d784f8a95
Mikołaj Kondela <mkondela.formal@gmail.com>
2024-07-19 21:47:30 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Background from "./components/Background";
+
+export default function App() {
+  return (
+    <div>
+      <Background />
+      <p>TEST</p>
+    </div>
+  );
+}
---
+++ src/components/Background.jsx
@@ -0,0 +1,7 @@
+export default function Background() {
+  return (
+    <div>
+      <img src="../images/background5" alt="background-mountains" />
+    </div>
+  );
+}

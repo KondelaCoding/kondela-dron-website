9d40a7aafdc113727fbd7f37b73738cc645009b4
Mikołaj Kondela <mkondela.formal@gmail.com>
2024-08-15 22:22:22 +0200

    Adjust margin in Background

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -6,12 +6,12 @@ import Background5 from "../images/background-5.png";
 
 export default function Background() {
   return (
-    <div className="aspect-video w-screen relative mt-32">
+    <div className="aspect-video w-screen relative mt-20">
       <img src={Background5} alt="background-5" className="absolute top-0" />
       <img src={Background4} alt="background-4" className="absolute top-0" />
       <img src={Background3} alt="background-3" className="absolute top-0" />
       <div className="flex justify-center items-center">
-        <h1 className="text-center text-4xl font-black drop-shadow-[10px_10px_10px_rgba(0,0,0,0.5)] uppercase tablet:text-7xl tablet:mt-10 laptop:text-9xl laptop:mt-32 desktop:text-[10rem] desktop:mt-44">
+        <h1 className="text-center text-4xl font-black drop-shadow-[10px_10px_10px_rgba(0,0,0,0.5)] uppercase tablet:text-7xl tablet:mt-10 laptop:text-9xl laptop:mt-52 desktop:text-[10rem] desktop:mt-64">
           zmień <br />
           perspektywę
         </h1>
